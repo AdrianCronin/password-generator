aa7ee3a0c051aa37f6fcd28c0ab557452ccfc34c
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-05-16 10:34:58 -0700

    made variables and got the button to display the placeholder password

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//This function is called when generate button is clicked
+function generatePassword() {
+  var numbers = [];
+  var lowerCase = [];
+  var upperCase = [];
+  var specialChar = [];
+  var password = "test-password";
+
+  return password;
+}
+// create arrays with all the possible criteria 
+// create empty `password` string
+// After button is clicked prompt user for criteria
+// save length criteria into a variable
+// possibly create new array with selected criteria
+// randomly select 1 character from each selected criteria array and put into `password` string
+// use a for loop that iterates the remaining string length number of times
+    // every iteration select a random character from the possible criteria array and attach to `password` string
+
+
